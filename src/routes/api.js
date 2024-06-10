f2e7e177514b42ce6b6eab05554f317cf5eefeb3
PhamThanhTuanDev <phamthanhtuan.dev@gmail.com>
2024-06-10 21:24:24 +0700

    91. #76. Update User API

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,10 +5,13 @@ const routerAPI = express.Router();
 const {
     getUsersAPI,
     postCreateUserAPI,
+    putUpdateUserAPI,
 } = require("../controllers/apiController");
 
 routerAPI.get("/users", getUsersAPI);
 
 routerAPI.post("/users", postCreateUserAPI);
+
+routerAPI.put("/users", putUpdateUserAPI);
 
 module.exports = routerAPI;
