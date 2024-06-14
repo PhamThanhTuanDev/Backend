a665a4e5ba3fa01a2f0d41925091ec68b269c954
PhamThanhTuanDev <phamthanhtuan.dev@gmail.com>
2024-06-14 23:47:42 +0700

    101. #86. Create a customer API

--- src/routes/api.js
+++ src/routes/api.js
@@ -11,16 +11,15 @@ const {
     postuploadMultipleFilesAPI,
 } = require("../controllers/apiController");
 
+const { postCreateCustomer } = require("../controllers/customerController");
 routerAPI.get("/users", getUsersAPI);
-
 routerAPI.post("/users", postCreateUserAPI);
-
 routerAPI.put("/users", putUpdateUserAPI);
-
 routerAPI.delete("/users", deleteUserAPI);
 
 routerAPI.post("/file", postUploadSingleFileAPI);
-
 routerAPI.post("/files", postuploadMultipleFilesAPI);
+
+routerAPI.post("/customers", postCreateCustomer);
 
 module.exports = routerAPI;
